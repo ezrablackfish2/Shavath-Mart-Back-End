8e75d1936989c90e252c12c6e678257ed6aa0a84
Alazar-dev <mealazartadesse@gmail.com>
2023-10-26 20:59:55 +0300

    productId

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = new mongoose.Schema({
+  productId: {
+    type: String,
+    required: true,
+  },
   message: {
     type: String,
     required: true,
