e0efe527bd4d53b68d82fafd50770416cfd31ba6
ezra <ezrayeneneh1992@gmail.com>
2023-11-03 06:13:51 +0000

    cors origin update

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
 const cookieParser = require("cookie-parser");
 
-const authRoute = require("./routes/auth")
-const productRoute = require("./routes/product")
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
-const connectDb = require("./config/db")
+const connectDb = require("./config/db");
 
 const app = express();
-connectDb()
+connectDb();
 
-// const corsOptions = {
-//   origin: 'http://localhost:3001',
-//   origin: 'http://localhost:3000',
-//   origin: 'https://shavath.vercel.app/',
-//   optionsSuccessStatus: 200, 
-// };
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
 
-app.use(cors());
-app.use(cookieParser())
+app.use(cors(corsOptions));
+
+
+app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoute);
-app.use("/api", productRoute)
+app.use("/api", productRoute);
 
-app.listen(3000, ()=> {
-    console.log("The application is running on 3000")
-})
-
+app.listen(3000, () => {
+  console.log("The application is running on 3000");
+});
 
