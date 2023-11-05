84b2cb78d380027ad205428b1e949212af0ee739
ezra <ezrayeneneh1992@gmail.com>
2023-11-05 08:57:15 +0000

    mysql branch

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,54 @@
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
+//
+//
+//const connectDb = async()=>{
+//    await mongoose.connect("mongodb://localhost:27017/shavath", {
+//        useNewUrlParser: true,
+//        useUnifiedTopology: true
+//    }).then(()=> console.log("Db connected successfully"))
+//        .catch(err => console.log(err))
+//}
+//
+//module.exports =  connectDb
 
+const mysql = require('mysql2');
 
-const connectDb = async()=>{
-    await mongoose.connect("mongodb://localhost:27017/shavath", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(()=> console.log("Db connected successfully"))
-        .catch(err => console.log(err))
-}
+const connectDb = async () => {
+  const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'ezra',
+    password: 'tutu@12345',
+    database: 'ezra',
+  });
 
-module.exports =  connectDb
+  db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+    console.log('Connected to MySQL');
+  });
+
+  // You can use the `db` connection to execute SQL queries in your application.
+
+  // For example, you can create a table (if it doesn't exist) using SQL like this:
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS ezra (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255),
+      description TEXT
+    )
+  `;
+
+  db.query(createTableQuery, (err, results) => {
+    if (err) {
+      console.error('Error creating table:', err);
+      return;
+    }
+	  console.log("Table created");
+
+  });
+};
+
+module.exports = connectDb;
+
--- src/models/Review.js
+++ src/models/Review.js
@@ -13,3 +13,4 @@ const ReviewSchema = new mongoose.Schema({
 
 const Review = mongoose.model("Review", ReviewSchema);
 module.exports = Review;
+
